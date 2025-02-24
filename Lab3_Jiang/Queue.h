0d8aa2eb9bc3ca10bbcf72d6d53a86298ebd0a2e
michaeljiang02 <144136141+michaeljiang02@users.noreply.github.com>
2025-02-24 08:30:59 -0500

    Update Queue.h

--- Lab3_Jiang/Queue.h
+++ Lab3_Jiang/Queue.h
@@ -1,8 +1,8 @@
 /****************************************************************************\
  * Queue.h
  *
- *  Created on:
- *      Author: YOUR NAME
+ *  Created on: Feb 13 2025
+ *      Author: Michael Jiang
  *
  *
  *  Implementation details: ?
@@ -22,11 +22,9 @@ private:
 		int m_MaxSize;
 		/** Max capacity of the circular buffer */
 		int m_First;
-		/** Index of the circular buffer before the first item of the queue. */
+		/** Index of the first item of the queue */
 		int m_Last;
-        /** Index of the circular buffer in which the next item will be queued.
-         * IMPORTANT: This is not the index of the last item of the queue.
-         */
+	        /** Index of the last item of the queue */
 		int length;
 		/** Number of items currently inside the queue. */
 
@@ -38,14 +36,16 @@ private:
 
 		/** Indicates whether of not the queue is empty in O(1), true if empty, false if not. */
 		bool IsEmpty();
-        /** Indicates whether of not the queue is full in O(1), true if full, false if not. */
-        bool IsFull();
+
+	        /** Indicates whether of not the queue is full in O(1), true if full, false if not. */
+	        bool IsFull();
 
 		/** Prints the content of the queue on a single line, separated by comma, eg: [3, 19, 2, 36]. */
 		void PrintQueue();
 
 		/** Inserts an element at the end of the queue, returns true if succeed, and false if the queue is already full. */
 		bool Enqueue(int new_value);
+
 		/** Removes the element at the front of the queue and returns the element in the argument,
 		 * returns true if succeed, and false if the queue is already full.  */
 		bool Dequeue(int &old_value);
